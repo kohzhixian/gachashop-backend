0968fe1ab533c27bb47fa6ea37cd076efed22644
kohzhixian <zhixianwork@gmail.com>
2022-12-06 11:23:16 +0800

    modified genre controller and routes

--- Routes/genre-routes.js
+++ Routes/genre-routes.js
@@ -4,8 +4,9 @@ const genreController = require('../controllers/genre-controller');
 const router = express.Router();
 
 
-router.get('/:genre', genreController.getGenreById);
-router.post('/createdGenre', genreController.createGenre);
-router.patch('/:genre', genreController.updateGenre);
+router.get('/:genre', genreController.getGamesByGenre);
+router.post('/addgame', genreController.addGameToGenre);
+router.patch('/updategame/:gameId', genreController.updateGames);
+router.delete('/deletegame/:gameId', genreController.deleteGames);
 
 module.exports = router;
--- controllers/genre-controller.js
+++ controllers/genre-controller.js
@@ -5,44 +5,66 @@ const DUMMY_GENRES = [
   {
     gid: "g1",
     genre: "Action",
-    games: {
-      id: "game1",
-      game_name: "epic seven",
-      image_url: "/Images/Action_epicseven.jpg",
-      price: 10,
-    },
+    games: [
+      {
+        id: "game1",
+        game_name: "epic seven",
+        image_url: "/Images/Action_epicseven.jpg",
+        price: 10,
+      },
+    ],
   },
+  {
+    gid: "g2",
+    genre: "Adventure",
+    games: [
+      {
+        id: "game2",
+        game_name: "botworld adventure",
+        image_url: "/Images/Adventure_botworldadventure.jpg",
+        price: 20,
+      },
+    ]
+  }
 ];
 
-const getGenreById = (req, res, next) => {
+const getGamesByGenre = (req, res, next) => {
   const genreType = req.params.genre;
-  const genre = DUMMY_GENRES.find((g) => {
-    return g.genre === genreType;
-  });
+  const genre = DUMMY_GENRES.filter(g => g.genre === genreType);
+  res.json({games: genre});
+}
 
-  if (!genre) {
-    return next(new HttpError("Could not find any data for genre id", 404));
-  }
-  res.json({ genre });
-};
+const addGameToGenre = (req, res, next) => {
+  const {genre, games} = req.body;
 
-const createGenre = (req, res, next) => {
-  const { gid, genre, gameDetails } = req.body;
-  const createGenre = {
-    id: uuid(),
-    gid,
-    genre,
-    games: gameDetails,
-  };
+  const genreType = DUMMY_GENRES.find(g => g.genre === genre);
+  genreType.games.push(games);
+  res.status(201).json({message: 'Game Added'});
+}
 
-  DUMMY_GENRES.push(createGenre);
-  res.status(201).json({ genre: createGenre });
-};
+const updateGames = (req, res, next) => {
+  const {price} = req.body;
+  const gameId = req.params.gameId;
 
-const updateGenre = (req, res, next) => {
-  
-};
+  DUMMY_GENRES.forEach((genre,idx) => {
+    const gameIdx = genre.games.findIndex(game => game.id === gameId);
+    if(gameIdx >= 0){
+      DUMMY_GENRES[idx].games[gameIdx].price = price;
+    }
+  })
+  res.status(200).json({message: "game updated"});
 
-exports.getGenreById = getGenreById;
-exports.createGenre = createGenre;
-exports.updateGenre = updateGenre;
+}
+
+const deleteGames = (req, res, next) => {
+  const gameId = req.params.gameId;
+  DUMMY_GENRES.forEach((genre, idx) => {
+    DUMMY_GENRES[idx].games = genre.games.filter(game => game.id !== gameId);
+  })
+  res.status(200).json({message: "Game deleted"});
+}
+
+exports.getGamesByGenre = getGamesByGenre;
+exports.addGameToGenre = addGameToGenre;
+exports.updateGames = updateGames;
+exports.deleteGames = deleteGames;
